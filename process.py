d401f8a15f104b82e427da89e71784462bb4409d
aram rafeq <aramrafeq2@gmail.com>
2020-10-12 12:38:08 +0300

    binarization step is done even better than before

--- process.py
+++ process.py
@@ -1,7 +1,14 @@
-from skimage import data
+from skimage import io
+from skimage import color
 from skimage import viewer
+from skimage import filters
 #proof of concept for skimage
-coffe = data.coffee()
-imgviewr = viewer.ImageViewer(coffe)
+imgPath = "./images/3.jpg"
+img = io.imread(imgPath, as_gray=True)
+# binarize input image and apply local theresould
+adaptiveThresh = filters.thresholding.threshold_sauvola(img,window_size=75 )
+binarizedImage = img >= adaptiveThresh
+
+imgviewr = viewer.ImageViewer(binarizedImage)
 imgviewr.show()
 
