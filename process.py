7530ee63641539e8fe38cddb832fe8b74f26c944
aram rafeq <aramrafeq2@gmail.com>
2020-10-12 21:58:37 +0300

    pre process reacreated even better

--- process.py
+++ process.py
@@ -2,13 +2,18 @@ from skimage import io
 from skimage import color
 from skimage import viewer
 from skimage import filters
+from skimage import transform
+from functions import deskew
 #proof of concept for skimage
-imgPath = "./images/3.jpg"
+imgPath = "./images/4.jpg"
 img = io.imread(imgPath, as_gray=True)
 # binarize input image and apply local theresould
-adaptiveThresh = filters.thresholding.threshold_sauvola(img,window_size=75 )
+adaptiveThresh = filters.thresholding.threshold_sauvola(img,window_size=71 )
 binarizedImage = img >= adaptiveThresh
 
-imgviewr = viewer.ImageViewer(binarizedImage)
+# Fixing document skew
+rotationAngle = deskew(binarizedImage)
+fixedImage = transform.rotate(binarizedImage, rotationAngle, cval=1, mode="constant")
+fixedImage2 =  fixedImage * 255
+imgviewr = viewer.ImageViewer(fixedImage2)
 imgviewr.show()
-
