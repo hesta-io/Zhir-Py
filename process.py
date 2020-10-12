ced4ef7f4f74e7f0904de2107efd1bda4c78d906
aram rafeq <aramrafeq2@gmail.com>
2020-10-12 22:34:16 +0300

    CLI is now working

--- process.py
+++ process.py
@@ -4,16 +4,24 @@ from skimage import viewer
 from skimage import filters
 from skimage import transform
 from functions import deskew
-#proof of concept for skimage
-imgPath = "./images/4.jpg"
-img = io.imread(imgPath, as_gray=True)
-# binarize input image and apply local theresould
-adaptiveThresh = filters.thresholding.threshold_sauvola(img,window_size=71 )
-binarizedImage = img >= adaptiveThresh
+from pathlib import Path
+import plac
 
-# Fixing document skew
-rotationAngle = deskew(binarizedImage)
-fixedImage = transform.rotate(binarizedImage, rotationAngle, cval=1, mode="constant")
-fixedImage2 =  fixedImage * 255
-imgviewr = viewer.ImageViewer(fixedImage2)
-imgviewr.show()
+@plac.opt('output_file', "Optional output file", type=Path)
+@plac.opt('input_file', "Input file", type=Path)
+def main(input_file, output_file='.'):
+    # imgPath = "./images/5-rotated.jpg"
+    imgPath = input_file
+    img = io.imread(imgPath, as_gray=True)
+    # binarize input image and apply local theresould
+    adaptiveThresh = filters.thresholding.threshold_sauvola(img,window_size=71 )
+    binarizedImage = img >= adaptiveThresh
+
+    # Fixing document skew
+    rotationAngle = deskew(binarizedImage)
+    fixedImage = transform.rotate(binarizedImage, rotationAngle, cval=1, mode="constant")
+    finalImage =  fixedImage * 255
+    io.imsave(output_file,finalImage)
+
+if __name__ == '__main__':
+    plac.call(main)
