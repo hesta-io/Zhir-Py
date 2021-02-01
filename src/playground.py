db9a3c3107b03c686c1fd95c98627f7bb7a762ba
aram rafeq <aramrafeq2@gmail.com>
2021-02-01 13:03:11 +0300

    playground.py contains something useful

--- src/playground.py
+++ src/playground.py
@@ -2,11 +2,49 @@ import zhirpy as zp
 import cv2
 import numpy as np
 from skimage import io
+from zhirpy import removeShadows
 
-img = cv2.imread('../images/wraped4.jpg', 0)
+def removeSmallObjects(img):
+    #find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=8)
+    #connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    #the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
+    sizes = stats[1:, -1]; nb_components = nb_components - 1
 
-cv2.imshow("after",  cv2.resize(img, (540, 960 )) ) 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# io.imsave('./after.jpg', img)
- 
+    # minimum size of particles we want to keep (number of pixels)
+    #here, it's a fixed value, but you can set it as you want, eg the mean of the sizes or whatever
+    min_size = 150  
+
+    #your answer image
+    img2 = np.zeros((output.shape))
+    #for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+            img2[output == i + 1] = 255
+
+
+large = removeShadows(cv2.imread('../images/4.jpg'))
+rgb = cv2.pyrDown(large)
+small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (13, 13))
+grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+
+_, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+# using RETR_EXTERNAL instead of RETR_CCOMP
+contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+mask = np.zeros(bw.shape, dtype=np.uint8)
+
+for idx in range(len(contours)):
+    x, y, w, h = cv2.boundingRect(contours[idx])
+    mask[y:y+h, x:x+w] = 0
+    cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+    r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+
+    if r > 0.45 and w > 8 and h > 8:
+        cv2.rectangle(rgb, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+cv2.imwrite("result.jpg",rgb)
